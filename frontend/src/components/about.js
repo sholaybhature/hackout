667ca7558170e3fc063d3ad679a0d741ecb21ad5
Ayan Jain <ayan.jain@gmail.com>
2020-11-07 15:48:47 +0530

    added image

---
+++ frontend/src/components/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MiniDrawer from './navbar'
+
+export default function About() {
+    return (
+        <div className = "grid-about">
+            <div className = "grid-item">
+                <MiniDrawer />
+            </div>
+        </div>
+    )
+}
